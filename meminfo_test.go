f1c48eca29ee01575c43429f98bdd8f9449e1a38
David Neumann <dane@luise>
2013-12-05 20:58:02 +0100

    moved some stuff

---
+++ meminfo_test.go
@@ -0,0 +1,13 @@
+package pstat
+
+import (
+	"testing"
+)
+
+func TestMemInfo(t *testing.T) {
+	info, err := ReadMemInfo("proc/meminfo")
+	if err != nil {
+		t.Fatal("meminfo read fail")
+	}
+	t.Logf("%+v", info)
+}
